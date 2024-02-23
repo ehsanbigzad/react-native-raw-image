81c478b75ff5542e35203092d1ccc58297599a4f
Mostasim Billah <mostasim.10@gmail.com>
2024-02-23 17:26:29 +0600

    feat: add ios support to convert .dng to .png file

--- ios/RawImage.swift
+++ ios/RawImage.swift
@@ -1,8 +1,41 @@
 @objc(RawImage)
 class RawImage: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+    
+    @objc(rawToPng:withResolver:withRejecter:)
+    func rawToPng(path: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        let imageURL = URL(fileURLWithPath: path)
+        let imageData = try? Data(contentsOf: imageURL)
+        if let imageData = imageData, let image = UIImage(data: imageData) {
+            let outputPath = convertToPNG(image: image)
+            if ((outputPath?.hashValue) != nil){
+                resolve(outputPath)
+            }
+            else {
+                reject("Error converting image", "IMAGE_CONVERTION_ERROR", nil)
+            }
+        } else {
+            reject("Error loading image from path", "IMAGE_LOADING_ERROR", nil)
+        }
+    }
+    
+    func convertToPNG(image: UIImage) -> String? {
+        var outputPath : String? = nil
+        if let jpegImage = image.pngData(){
+            
+            do {
+                //Convert
+                let tempDirectoryURL = URL(fileURLWithPath:  NSTemporaryDirectory(), isDirectory: true)
+                let newFileName = "\(UUID().uuidString).png"
+                let targetURL = tempDirectoryURL.appendingPathComponent(newFileName)
+                try jpegImage.write(to: targetURL, options: [])
+                outputPath = targetURL.relativePath
+            }catch {
+                print (error.localizedDescription)
+                print("FAILED")
+            }
+        }else{
+            print("FAILED")
+        }
+        return outputPath
+    }
 }
